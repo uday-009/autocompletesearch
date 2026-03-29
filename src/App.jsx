320a91edebc559a7bb9b72467f61a08df4cf0614
Uday Maroju <udaymaroju9@gmail.com>
2026-03-29 21:58:23 +0530

    autocomplete search basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import heroImg from './assets/hero.png'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+  const [cache, setCache] = useState({});
+  const fetchData = async () => {
+    if(cache[input]){
+      setResults(cache[input]);
+      return;
+    }
+    const data = await fetch('https://dummyjson.com/recipes/search?q=' + input)
+    const json = await data.json();
 
-  return(<div className="App">
+    setResults(json?.recipes);
+    setCache(prev => ({...prev, [input]: json?.recipes}))
+
+  }
+  useEffect(() => {
+    const timer  = setTimeout(fetchData, 500);
+    return () => {
+      console.log('clearing timer')
+  clearTimeout(timer);
+    } 
+  
+  }, [input])
+
+  return (<div className="App">
     <div className="container">
       <div className="left">
-        <h1>Discover Your Next Favorite Book</h1>
-        <p>Explore our vast collection of books and find your next great read. Whether you're into fiction, non-fiction, or anything in between, we've got something for everyone.</p>
-        <button>Browse Books</button>
+        <h1>Auto complete search</h1>
+        <input name='searchInput' type='text'
+          className='search-input' value={input} onChange={(e) => setInput(e.target.value)}
+          onFocus={() => setShowResults(true)}
+          onBlur={() => setShowResults(false)}
+        />
+        {showResults && <div className='results-container'>
+          {results.length ? results.map((r) => <span className='result' key={r.id}>{r.name}</span>) : <span className='result' >{"No results"}</span> }
+        </div>}
       </div>
-      <div className="right">
-        <img src={heroImg} alt="Hero" />
-      </div>
+
     </div>
   </div>)
 }
